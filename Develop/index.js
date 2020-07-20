1f16b9be1110be373ab8e1064311b03ddaf5a4db
osborn roxas <osbornroxas02@gmail.com>
2020-07-20 05:53:56 -0700

    re coded

--- Develop/index.js
+++ Develop/index.js
@@ -1,18 +1,88 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
+const promptUser = inquirer
+
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        message: "What is your project title?",
+        name: "Title"
+    },
+    {
+        type: "input",
+        message: "Describe your project",
+        name: "Description"
+    },
+    {
+        type: "input",
+        message: "What are the steps required to install your project?",
+        name: "Installation"
+    },
+    {
+        type: "input",
+        message: "Provide instructions for use.",
+        name: "Usage"
+    },
+    {
+        type: "input",
+        message: "Contributors?",
+        name: "Contributors"
+    },
+    {
+        type: "input",
+        message: "How do you test your project?",
+        name: "Test"
+    },
+    {
+        type: "list",
+        message: "Select license",
+        name: "License",
+        choices: [
+            "MIT",
+            "GVL-GPL 3.0",
+            "APACHE 2.0",
+            "BSD 3",
+            "None"
+        ]
+    },
+    {
+        type: "input",
+        message: "Your Github username",
+        name: "Github"
+    },
+    {
+        type: "input",
+        message: "What is your email?",
+        name: "Email"
+    }
 
 ];
 
-// function to write README file
+// Function to write file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            throw err;
+        }
+    });
 }
+
 
 // function to initialize program
 function init() {
+    promptUser(questions).then(answers => {
+
+        const response = generateMarkdown(answers);
+        console.log(answers);
+
+        writeToFile("README.md", response);
+
+    })
 
 }
 
 // function call to initialize program
 init();
-
-
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,11 +1,46 @@
-function to generate markdown for README
+// function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  
+# ${data.Title}
+
+## Table of Contents
+
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [Credits](#Credits)
+* [License](#License)
+
+## Description 
+${data.Description}
+
+## Installation
+${data.Installation}
+
+## Usage 
+${data.Usage}
+
+## Credits
+${data.Credits}
+
+## License
+${data.License}
+
+## Contributors
+${data.Contributors}
+
+## Test
+${data.Test}
+
+## Author
+Github:[${data.Github}](https://github.com/osbornroxas02)
+Email:[${data.Email}](https://github.com/osbornroxas02)
 
 `;
 }
 
 module.exports = generateMarkdown;
+
 
 
 
